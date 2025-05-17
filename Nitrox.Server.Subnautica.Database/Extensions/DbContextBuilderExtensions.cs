59468eb4bf2b43e0ee1d9aa3e9ca42962162eeec
Measurity <1107063+Measurity@users.noreply.github.com>
2025-05-17 12:47:52 +0200

    Disabled PRAGMA commands from EF
    PRAGMA is managed by server instead of EF

--- Nitrox.Server.Subnautica.Database/Extensions/DbContextBuilderExtensions.cs
+++ Nitrox.Server.Subnautica.Database/Extensions/DbContextBuilderExtensions.cs
@@ -12,6 +12,6 @@ public static class DbContextBuilderExtensions
         {
             infraOptions.AddOrUpdateExtension(new NitroxDbContextOptionsExtension());
         }
-        options.AddInterceptors(new AddSqliteFunctionsInterceptor());
+        options.AddInterceptors(new DisableInternalEntityFrameworkPragmaInterceptor(), new AddSqliteFunctionsInterceptor());
     }
 }
